b5878573c8a36efad334cbdc1ebd98af9be15e65
Hyowon Kim <hyopangworld@gmail.com>
2019-09-25 10:45:11 +0900

    Create new main VC
    To remake project for following with 'raywenderlich' tutorial

---
+++ TodoList/Source/TodoListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TodoListViewController.swift
+//  TodoList
+//
+//  Created by 김효원 on 25/09/2019.
+//  Copyright © 2019 HyowonKim. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoListItem", for: indexPath)
+        return cell
+    }
+}
